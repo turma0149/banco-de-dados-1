3692211c94ce6d55aeb819fcdff88bb9c42967b6
Michelle Belli <progweb0149@gmail.com>
2026-08-24 19:54:24 -0300

    Atualizar

--- script_exercicios_create_insert.sql
+++ script_exercicios_create_insert.sql
@@ -58,5 +58,59 @@ INSERT INTO produto (nome,categoria,preco,quantidade)
 	VALUES ('Bacon','Alimento','5.00','200');
     
 INSERT INTO produto (nome,categoria,preco,quantidade)
-	VALUES ('Caderno','Papelaria','50.00','2')
+	VALUES ('Caderno','Papelaria','50.00','2');
 
+-- ---------------------------------------------------
+
+CREATE DATABASE ex04_escola;
+USE ex04_escola;
+
+CREATE TABLE curso(
+	id_curso     INT   AUTO_INCREMENT    PRIMARY KEY, 
+    nome         VARCHAR (100),
+    categoria    VARCHAR (50)
+);
+
+CREATE TABLE aluno(
+	id_aluno    INT   AUTO_INCREMENT  PRIMARY KEY,
+    nome        VARCHAR (100),
+    email       VARCHAR(100),
+    data_nasc   DATE,
+    id_curso    INT,
+    FOREIGN KEY (id_curso) REFERENCES curso (id_aluno)
+);
+    
+INSERT INTO curso(nome,categoria) 
+	VALUES('ADS', 'Tecnologia');
+    
+INSERT INTO curso(nome,categoria)
+   VALUES('Medicina', 'Saúde');
+    
+SELECT * FROM curso;
+
+ INSERT INTO aluno(nome,email,data_nasc,id_curso)
+	VALUES ('Maria Silva', 'ms@email.com','1984/11/25','1');
+    
+INSERT INTO aluno(nome,email,data_nasc,id_curso)
+	VALUES ('João Ferreira', 'jf@email.com','2005/02/22','1');
+    
+SELECT * FROM ALUNO
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+    
